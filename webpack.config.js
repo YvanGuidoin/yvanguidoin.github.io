9402f712724b4ae2fd8f9427384706dc1813eb9b
YvanGuidoin <yvan.guidoin@gmail.com>
2017-04-16 19:36:48 +0200

    Meta + modularize Webpack output for easier build tool

--- webpack.config.js
+++ webpack.config.js
@@ -3,57 +3,12 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin');
 
-module.exports = {
-  cache: true,
-  // devtool: 'eval',
-  devtool: 'cheap-source-map',
-  entry: './source/index.jsx',
-  output: {
-    path: path.resolve(__dirname, ''),
-    filename: "main.bundle.js",
-    pathinfo: true
-  },
-  resolve: {
-      extensions: ['.jsx', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: true,
-          presets: [['es2015', {modules: false, loose: true}], 'react'],
-          plugins: ['lodash', 'transform-runtime', 'transform-object-rest-spread', 'transform-react-inline-elements', 'transform-react-constant-elements']
-        }
-      },
-      {
-        test: /\.css$/,
-        exclude: /(node_modules|bower_components)/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-          { loader: "sass-loader" }
-        ]
-      },
-      {
-        test: /\.(jpe?g|png|gif|svg)$/i,
-        loader: "file-loader?name=/assets/[name].[ext]"
-      }
-    ]
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
+module.exports = function(env) {
+  let isProd = (env.production === 'PROD');
+  let devToolToUse = (isProd) ? 'cheap-source-map' : 'eval';
+
+  let pluginsProduction = [
+    new webpack.DefinePlugin({ 'process.env.NODE_ENV': JSON.stringify('production') }),
     new LodashModuleReplacementPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
@@ -71,11 +26,79 @@ module.exports = {
       },
       exclude: [/\.min\.js$/gi] // skip pre-minified libs
     }),
-    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ];
+
+  let commonsPlugins = [
     new HtmlWebpackPlugin({
       inject: true,
       template: 'source/index.html',
       favicon: 'source/assets/favicon.ico'
     })
-  ]
+  ];
+
+  let pluginsToUse;
+  if (isProd) pluginsToUse = pluginsProduction.concat(commonsPlugins);
+  else pluginsToUse = commonsPlugins;
+
+  return {
+    cache: true,
+    devtool: devToolToUse,
+    entry: './source/index.jsx',
+    output: {
+      path: path.resolve(__dirname, ''),
+      filename: "main.bundle.js",
+      pathinfo: true
+    },
+    resolve: {
+      extensions: ['.jsx', '.js']
+    },
+    module: {
+      rules: [{
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            cacheDirectory: true,
+            presets: [
+              ['es2015', {
+                modules: false,
+                loose: true
+              }], 'react'
+            ],
+            plugins: ['lodash', 'transform-runtime', 'transform-object-rest-spread', 'transform-react-inline-elements', 'transform-react-constant-elements']
+          }
+        },
+        {
+          test: /\.css$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [{
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader"
+            },
+          ]
+        },
+        {
+          test: /\.scss$/,
+          use: [{
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader"
+            },
+            {
+              loader: "sass-loader"
+            }
+          ]
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loader: "file-loader?name=/assets/[name].[ext]"
+        }
+      ]
+    },
+    plugins: pluginsToUse
+  }
 }
